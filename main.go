c6dc3c6fbdfda766bab39d001b7ebe12a7d53e6f
Ahmed Ashraf <a7med.ashraff@outlook.com>
2022-10-16 00:42:52 +0200

    feat(fix feature bug): change the printing string

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("hello, second version, here's new commit")
 	fmt.Println("do a second job")
-	fmt.Println("do a second job part two")
+	fmt.Println("do a second job part 2")
 }
