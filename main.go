b747aa6e8d97f345f7fd20f873c3e16c708ad084
Ahmed Ashraf <a7med.ashraff@outlook.com>
2022-11-01 16:59:46 +0200

    feat(standalone feature): asdf
    eree

--- main.go
+++ main.go
@@ -4,14 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello, second version, here's new commit")
-	fmt.Println("do a second job")
-	fmt.Println("do a second job part 2")
-
-	fmt.Println("feature B do a second job")
-	fmt.Println("feature B do a second job part 2")
-
-	fmt.Println("feature C do a second job")
-	fmt.Println("feature C do a second job part 2")
-	fmt.Println("feature C do a second job part 3")
-	fmt.Println("feature C do a second job part 3.1")
+	// test
 }
